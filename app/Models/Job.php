cc7313375b45e1a11ec7b17677170e87c414b9f6
Lian Paul Molo <lianpaulmolo08@gmail.com>
2025-04-28 09:46:28 +0800

    add pivot table for employer

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -9,12 +9,16 @@ use Illuminate\Database\Eloquent\Model;
 class Job extends Model{
     use HasFactory;
     
-    protected $table = "job_listing";
+    protected $table = "job_listings";
     protected $fillable = ['title','salary'];
 
     public function employer() {
         // Define the relationship with the Employer model
         return $this->belongsTo(Employer::class);
+    }
+
+    public function tags() {
+        return $this->belongsToMany(Tag::class, foreignPivotKey:'job_listing_id');
     }
 } 
 
--- database/migrations/2025_04_15_051608_create_job_listing_table.php
+++ database/migrations/2025_04_15_051608_create_job_listing_table.php
@@ -11,9 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('job_listing', function (Blueprint $table) {
+        Schema::create('job_listings', function (Blueprint $table) {
             $table->id(); 
-            // $table->unsignedBigInteger('employer_id');
             $table->foreignIdFor(\App\Models\Employer::class);
             $table->string('title');
             $table->string('salary');
@@ -26,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('job_listing');
+        Schema::dropIfExists('job_listings');
     }
 };
